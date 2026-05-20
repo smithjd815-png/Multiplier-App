84a090526c59b40531190575eb352bf3f1c92161
smithjd815-png <smithjd815@gmail.com>
2026-05-20 14:41:34 +0200

    changed minsdk to 26 for my emulator to work

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val num1 = findViewById<EditText>(R.id.num1)
+        val num2 = findViewById<EditText>(R.id.num2)
+        val btnMultiply = findViewById<Button>(R.id.btnMultiply)
+        val txtAnswer = findViewById<TextView>(R.id.txtAnswer)
+
+        // Added numbers
+        num1.setText("26")
+        num2.setText("52")
+
+        btnMultiply.setOnClickListener {
+
+            val number1 = num1.text.toString().toDouble()
+            val number2 = num2.text.toString().toDouble()
+
+            val result = multiplyNumbers(number1, number2)
+
+            txtAnswer.text = "Answer: $result"
         }
     }
-}
+
+    // multiply my numbers
+    fun multiplyNumbers(a: Double, b: Double): Double {
+        return a * b
+        }
+    }
